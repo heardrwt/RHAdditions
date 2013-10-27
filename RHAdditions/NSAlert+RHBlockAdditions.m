5ade5f8c5c1d3b2e35e2c3f87fbfd1c8f11b53fc
Richard Heard <richie@rheard.com>
2013-10-26 22:39:01 -0700

    Update NSAlert+RHBlockAdditions to take ARC compatibility into account. Include License.

--- RHAdditions/NSAlert+RHBlockAdditions.m
+++ RHAdditions/NSAlert+RHBlockAdditions.m
@@ -1,31 +1,56 @@
 //
 //  NSAlert+RHBlockAdditions.m
-//  Screenie
 //
 //  Created by Richard Heard on 18/10/2013.
 //  Copyright (c) 2013 Richard Heard. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//  3. The name of the author may not be used to endorse or promote products
+//  derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+//  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 #import "NSAlert+RHBlockAdditions.h"
+#import "RHARCSupport.h"
 
 @implementation NSAlert (RHBlockAdditions)
 
 -(void)rh_beginSheetModalForWindow:(NSWindow *)sheetWindow completionHandler:(void (^)(RHModalResponse returnCode))handler{
+    if (handler) handler = [handler copy];
+
     //use the provided block method if available
 #if defined(MAC_OS_X_VERSION_10_9) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9)
     if ([self respondsToSelector:@selector(beginSheetModalForWindow:completionHandler:)]){
         [self beginSheetModalForWindow:sheetWindow completionHandler:handler];
         return;
     }
-#endif
+#endif //end 10.9+
     
-    [self beginSheetModalForWindow:sheetWindow modalDelegate:self didEndSelector:@selector(rh_alertDidEnd:returnCode:contextInfo:) contextInfo:(void *)CFBridgingRetain(handler)];
+    [self beginSheetModalForWindow:sheetWindow modalDelegate:self didEndSelector:@selector(rh_alertDidEnd:returnCode:contextInfo:) contextInfo:(__bridge_retained void *)handler];
 }
 
 -(void)rh_alertDidEnd:(NSAlert *)alert returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo{
     if (contextInfo){
-        void (^completionBlock)(RHModalResponse returnCode) = CFBridgingRelease(contextInfo);
+        void (^completionBlock)(RHModalResponse returnCode) = (__bridge_transfer void (^)(RHModalResponse))(contextInfo);
         completionBlock(returnCode);
+        arc_release(completionBlock);
     }
 }
 
@@ -37,5 +62,9 @@
     return arc_autorelease(alert);
 }
 
-
 @end
+
+
+//include an implementation in this file so we don't have to use -load_all for this category to be included in a static lib
+@interface RHFixCategoryBugClassNSARHBA : NSObject @end @implementation RHFixCategoryBugClassNSARHBA @end
+
